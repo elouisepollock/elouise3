d4cbce8c6946a11f5a5451c20954f428498f3430
elouise <elouise.l.pollock@gmail.com>
2018-05-28 23:06:03 +1000

    added to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.compile = true
+  config.assets.digest = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -89,3 +90,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
