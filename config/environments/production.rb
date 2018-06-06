478f5ee9f3725b0cb187420c6d1abc40db6e6e80
elouise <elouise.l.pollock@gmail.com>
2018-06-06 16:09:08 +1000

    changed quotes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,10 +96,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "https://elouisepollock.herokuapp.com/" }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-  :user_name => ENV[‘SENDGRID_USERNAME’],
-  :password => ENV[‘SENDGRID_PASSWORD’],
-  :domain => ‘heroku.com’,
-  :address => ‘smtp.sendgrid.net’,
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
+  :domain => "heroku.com",
+  :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
