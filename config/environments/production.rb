2aa50f50106aaa9dc63b896a75868b45d6ea8c79
elouise <elouise.l.pollock@gmail.com>
2018-06-06 15:46:27 +1000

    coffee script added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,3 +94,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 end
 
+config.action_mailer.default_url_options = { host: ‘https://elouisepollock.herokuapp.com’ }
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+:user_name => ENV[‘SENDGRID_USERNAME’],
+:password => ENV[‘SENDGRID_PASSWORD’],
+:domain => ‘heroku.com’,
+:address => ‘smtp.sendgrid.net’,
+:port => 587,
+:authentication => :plain,
+:enable_starttls_auto => true
+}
