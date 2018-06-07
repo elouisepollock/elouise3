e39c7d8f06324855a7899238c50f3e4084bcb490
elouise <elouise.l.pollock@gmail.com>
2018-06-07 21:26:08 +1000

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'application#welcome'
+  root to: 'contacts#new'
   resources :contacts, only: [:new, :create]
-  # get '/welcome' => 'application#welcome'
-  # post '/contact' => '
+   get '/welcome' => 'application#welcome'
+  
 end
